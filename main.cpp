03a898e2afe9ab5bf3aab9e65f395913c13e157e
mcalbertkay007 <58604321+mcalbertkay007@users.noreply.github.com>
2019-12-06 18:23:38 +0100

    Inserimento dei commenti
    Agiunti vari commenti!

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+//Questo è un semplice programma C++ che ha il compito di mostrare nella console nera la scritta hello world
 
 int main() {
-	cout<<"hello world";
-	cin.get();
-	return 0;
+	cout<<"hello world"; //tutto ciò che è nei doppi apici è chiamata stringa! Tale stringa sara inviata nella console nera
+	cin.get();//aspetto finchè l'utente non preme qualche tasto!
+	return 0;//return significa che io resistuisco alla funzione il numero 0 che significa che tutto è andato bene!
 }
